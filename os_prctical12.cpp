f2ac7ff753e96fee25043a0d5f7e65fe6566bcb7
Gopal1tayade <88544678+Gopal1tayade@users.noreply.github.com>
2022-06-06 23:46:54 +0530

    Update os_prctical12.cpp

--- os_prctical12.cpp
+++ os_prctical12.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 int queue[20],n,head,i,j,k,seek=0,max,diff;
@@ -16,7 +17,7 @@ scanf("%d",&head);
 queue[0]=head;
 for(j=0;j<=n-1;j++)
 {
-diff=(queue[j+1]-queue[j]);
+diff= abs(queue[j+1]-queue[j]);
 seek+=diff;
 printf("Disk head moves from %d to %d withseek %d \n ",queue[j],queue[j+1],diff);
 }
